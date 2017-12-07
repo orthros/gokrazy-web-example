f6f25163970184bfb98b3790a454d1ec6ac8bece
orthros <colinnlsn@gmail.com>
2017-12-07 07:23:41 -0600

    Added some logs and changed the log in quitting to "Quitting!" (was "Exiting!")

--- gokrazy-web-example.go
+++ gokrazy-web-example.go
@@ -5,16 +5,18 @@ import "net/http"
 import "os"
 
 func greeting(w http.ResponseWriter, req *http.Request) {
+	log.Print("Greeting!")
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("Hello from a service run by gokrazy"))
 }
 
 func quitting(w http.ResponseWriter, req *http.Request) {
-	log.Print("Exiting!")
+	log.Print("Quitting!")
 	os.Exit(0)
 }
 
 func main() {
+	log.Print("Coming online!")
 	http.HandleFunc("/greet", greeting)
 	http.HandleFunc("/quit", quitting)
 	log.Fatal(http.ListenAndServe(":8080", nil))
