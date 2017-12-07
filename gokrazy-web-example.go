a4541e4e57496f4b67e423043c349baf8533ba88
orthros <colinnlsn@gmail.com>
2017-12-07 07:02:03 -0600

    Added a function that will "exit" the program: quitting Currently only writes to the response stream that it is quitting

--- gokrazy-web-example.go
+++ gokrazy-web-example.go
@@ -8,7 +8,13 @@ func greeting(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("Hello from a service run by gokrazy"))
 }
 
+func quitting(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Exiting the program!"))
+}
+
 func main() {
-    http.HandleFunc("/greet", Greeting)
-    log.Fatal(http.ListenAndServe(":8080",nil))
+	http.HandleFunc("/greet", greeting)
+	http.HandleFunc("/quit", quitting)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
