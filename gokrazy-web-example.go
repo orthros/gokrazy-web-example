4cc098553e832a9dc4c400dcf0c292f687e44b2d
orthros <colinnlsn@gmail.com>
2017-12-07 07:01:06 -0600

    Formatted the greeting function and made the function non-exported

--- gokrazy-web-example.go
+++ gokrazy-web-example.go
@@ -3,9 +3,9 @@ package main
 import "log"
 import "net/http"
 
-func Greeting(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "text/plain")
-    w.Write([]byte("Hello from a service run by gokrazy"))
+func greeting(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("Hello from a service run by gokrazy"))
 }
 
 func main() {
